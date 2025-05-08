ee4aa7f9416335805d4eee8410894098c7c4ba2c
Amitesh Sharma <108935281+ammysharma11@users.noreply.github.com>
2025-05-08 15:59:18 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -25,11 +25,15 @@ export default function App() {
      */
 
     function rollDice() {
-        setDice(oldDice => oldDice.map(die =>
-            die.isHeld ?
-                die :
-                { ...die, value: Math.ceil(Math.random() * 6) }
-        ))
+       if (!gameWon) {
+            setDice(oldDice => oldDice.map(die =>
+                die.isHeld ?
+                    die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            ))
+        } else {
+            setDice(generateAllNewDice())
+        }
     }
 
     function hold(id) {
